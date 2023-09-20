778e445213a789f7ace70d7b6556573c21e69d0f
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2023-09-20 18:39:12 +0300

    add some dependencies, and config file

---
+++ src/main/java/com/naukma/cleaning/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.naukma.cleaning.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@ComponentScan(basePackages = "com.naukma.cleaning")
+public class AppConfig {
+    @Bean
+    public PasswordEncoder getEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
