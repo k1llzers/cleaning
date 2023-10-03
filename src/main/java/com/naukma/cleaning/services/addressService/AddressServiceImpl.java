c8b027d7ea74237f91faf70fff9b377072d73523
Artemii Kolomiichuk <95016660+ArtemiiKolomiichuk@users.noreply.github.com>
2023-10-03 17:54:31 +0300

    update addressServiseImpl

--- src/main/java/com/naukma/cleaning/services/addressService/AddressServiceImpl.java
+++ src/main/java/com/naukma/cleaning/services/addressService/AddressServiceImpl.java
@@ -1,28 +1,42 @@
 package com.naukma.cleaning.services.addressService;
 
+import com.naukma.cleaning.dao.AddressDao;
+import com.naukma.cleaning.dao.entities.AddressEntity;
 import com.naukma.cleaning.models.order.AddressDto;
+
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AddressServiceImpl implements AddressService{
+    private AddressDao addressDao;
+    private ModelMapper modelMapper;
+
+    public AddressServiceImpl(AddressDao addressDao, ModelMapper modelMapper) {
+        this.addressDao = addressDao;
+        this.modelMapper = modelMapper;
+    }
+
     @Override
     public void createAddress(AddressDto addressDto) {
-        
-
+        AddressEntity addressEntity = modelMapper.map(addressDto, AddressEntity.class);
+        addressDao.save(addressEntity);
     }
 
     @Override
     public void editAddress(AddressDto addressDto) {
-
+        AddressEntity addressEntity = modelMapper.map(addressDto, AddressEntity.class);
+        addressDao.save(addressEntity);
     }
 
     @Override
     public void deleteAddress(long id) {
-
+        addressDao.deleteById(id);
     }
 
     @Override
     public AddressDto getAddress(long id) {
-        return null;
+        AddressEntity addressById = addressDao.getReferenceById(id);
+        return modelMapper.map(addressById,AddressDto.class);
     }
 }
