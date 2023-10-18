b4a309fb66bef3392bc97e515b5c54def33f28f0
Danylo Shlapak <geografy2314@gmail.com>
2023-10-18 20:18:26 +0300

    Added validation annotations for UserDto and OrderDto

--- src/main/java/com/naukma/cleaning/models/user/UserDto.java
+++ src/main/java/com/naukma/cleaning/models/user/UserDto.java
@@ -1,6 +1,6 @@
 package com.naukma.cleaning.models.user;
 
-import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
@@ -16,11 +16,19 @@ import lombok.ToString;
 public class UserDto {
     private long id;
     @NonNull
+    @NotBlank
+    @NotNull(message = "Name cannot be null")
     private String name;
     @NonNull
+    @NotBlank
+    @Size(min = 12, message = "Password is too short!")
+    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).+$", message = "Password must contain at least one uppercase letter, one lowercase letter, and one digit")
     private String password;
     @NonNull
+    @NotNull(message = "Email cannot be null")
+    @Email(message = "Email should be valid")
     private String email;
     @NonNull
+    @NotNull(message = "Role cannot be null")
     private Role role;
 }
