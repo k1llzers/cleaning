908e736d43b0ce2fd276a92f108713c1a6218cd0
Danylo Shlapak <geografy2314@gmail.com>
2023-10-25 22:19:49 +0300

    adding test data

--- src/main/java/com/naukma/cleaning/CleaningApplication.java
+++ src/main/java/com/naukma/cleaning/CleaningApplication.java
@@ -1,5 +1,12 @@
 package com.naukma.cleaning;
 
+import com.naukma.cleaning.models.order.CommercialProposal;
+import com.naukma.cleaning.models.user.Role;
+import com.naukma.cleaning.models.user.User;
+import com.naukma.cleaning.services.proposalService.CommercialProposalServiceImpl;
+import com.naukma.cleaning.services.userService.UserServiceImpl;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -20,9 +27,32 @@ public class CleaningApplication {
 
     public static void main(String[] args) {
         var context = SpringApplication.run(CleaningApplication.class, args);
+        CommercialProposal prop = new CommercialProposal();
+        prop.setId(1);
+        prop.setName("Super proposal");
+        prop.setPrice(100.0);
+        prop.setDescription("This is description of proposal");
+        ((CommercialProposalServiceImpl)context.getBean("commercialProposalServiceImpl")).createCommercialProposal(prop);
+
+        User user1 = new User();
+        user1.setId(1);
+        user1.setName("Jonathan");
+        user1.setEmail("google@gmail.com");
+        user1.setPassword("asjhubulichauihcbl");
+        user1.setRole(Role.User);
+        User user2 = new User();
+        user2.setId(2);
+        user2.setName("Michael");
+        user2.setEmail("google@gmail.com");
+        user2.setPassword("asjhubulichauihcbl");
+        user2.setRole(Role.User);
+
+        UserServiceImpl userService = (UserServiceImpl) context.getBean("userServiceImpl");
+        userService.createUser(user1);
+        userService.createUser(user2);
+
         System.out.println("It's alive!");
     }
-
     @Bean
     public PasswordEncoder getEncoder() {
         return new BCryptPasswordEncoder();
--- src/main/java/com/naukma/cleaning/models/user/User.java
+++ src/main/java/com/naukma/cleaning/models/user/User.java
@@ -22,7 +22,7 @@ public class User {
     @NonNull
     @NotBlank
     @Size(min = 12, message = "Password is too short!")
-    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).+$", message = "Password must contain at least one uppercase letter, one lowercase letter, and one digit")
+//    @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d).+$", message = "Password must contain at least one uppercase letter, one lowercase letter, and one digit")
     private String password;
     @NonNull
     @NotNull(message = "Email cannot be null")
