391bbb0ee7f5602bdb699ea1d1ef7e898c1fe047
Oleksandr Semytskyi <ssemitskiy@gmail.com>
2023-10-25 19:23:17 +0300

    add validation

--- src/main/java/com/naukma/cleaning/models/dtos/CommercialProposalDto.java
+++ src/main/java/com/naukma/cleaning/models/dtos/CommercialProposalDto.java
@@ -1,5 +1,8 @@
 package com.naukma.cleaning.models.dtos;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -7,7 +10,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class CommercialProposalDto {
     private long id;
+    @NotBlank
+    @NotNull(message = "Name cannot be null")
     private String name;
     private String description;
+    @Positive
     private double price;
 }
